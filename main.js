171b18145d3d862c6d200f0e73e208a06aad4ef5
bastardicienta <latataalvarez1089@hotmail.com>
2021-10-09 00:04:31 -0500

    hasta media noche

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function message(name){
 function add(num1,num2){
     if(typeof num1 != 'number' || typeof num2 != 'number'){
         console.log("Sólo ingresa números, por favor");
-    }else{
+    }
+    else{
     console.log("El resultado es: " + (Number(num1) + Number(num2)));
     }
 }
@@ -21,7 +22,8 @@ function add(num1,num2){
 function subtract(num1,num2){
     if(typeof num1 != 'number' || typeof num2 != 'number'){
         console.log("Sólo ingresa números, por favor");
-    }else{
+    }
+    else{
     console.log("El resultado es: " + (Number(num1) - Number(num2)));
     }
 }
@@ -32,7 +34,8 @@ function subtract(num1,num2){
 function multi(num1,num2){
     if(typeof num1 != 'number' || typeof num2 != 'number'){
         console.log("Sólo ingresa números, por favor");
-    }else{
+    }
+    else{
     console.log("El resultado es: " + (Number(num1) * Number(num2)));
     }
 }
@@ -43,10 +46,91 @@ function multi(num1,num2){
 function divide(num1,num2){
     if(typeof num1 != 'number' || typeof num2 != 'number'){
         console.log("Sólo ingresa números, por favor");
-    }else{
+    }
+    else{
     console.log("El resultado es: " + (Number(num1) / Number(num2)));
     }
 }
-//divide(16,-8)
+//divide(16,0.5)
 
 // Escribe una función que reciba el radio de un círculo como parámetro, calcule el área del círculo e imprima en consola.
+
+function circleArea(radio) {
+    const pi=Math.PI
+    if(typeof radio != 'number'){
+        console.log("No lo hagas más difícil, amigo :'v");
+    }
+    else{
+    console.log("El área del círculo es: " + (pi * Math.pow(radio,2)));
+    }
+}
+//circleArea(6)
+
+// Escribe una función que reciba como parámetro un valor en metros e imprima en consola su equivalente en kilómetros, con máximo dos decimales.
+
+function convertLength(meters){
+    if(typeof meters != 'number'){
+        console.log("Sólo ingresa números, por favor");
+    }
+    else{
+        console.log(meters + " metros es igual a " + (meters/1000).toFixed(2) +" kilómetros")
+    }
+}
+//convertLength(1000)
+
+// Escribe una función que reciba como parámetro un valor en grados Celsius e imprima en consola su equivalente en grados Farenheit, con máximo dos decimales.
+
+function convertTemp(Celsius){
+    if(typeof Celsius != 'number'){
+        console.log("Sólo ingresa números, por favor");
+    }
+    else{
+        console.log(Celsius + "° Celsius es igual a " + ((Celsius*1.8)+32).toFixed(2) + "° Fahrenheit")
+    }
+}
+//convertTemp(5)
+
+// Escribe una función que reciba las siguientes listas de números como parámetro y encuentre el valor más alto y más bajo en la lista. Lista1 = [10,4,2,20,5] Lista2=[0,0,0,1,0] Lista3=[-13,10,9,8,0,2000,-1]
+
+//Con este me hice bolita
+
+// Escribe una función que reciba dos parámetros con los que se calculará el valor exponencial de una base. El primer parámetro será la base y el segundo será el exponente. Imprimir el resultado del valor exponencial de la base en consola.
+
+function exponential(base,exponent){
+    if(typeof base !='number' || typeof exponent != 'number'){
+        console.log("Sólo ingresa números, por favor");
+    }
+    else{
+        console.log("El valor exponencial es " + Math.pow(base,exponent))
+    }
+}
+exponential(3,2)
+
+//Escribe una función que reciba un número como parámetro. Este número será la cantidad de veces que se debe solicitar, con la función prompt, que el usuario ingrese el nombre de un país diferente. Después de que el usuario ingrese el nombre de los países, el sistema debe mostrar un mensaje en pantalla indicando el nombre del país y cuántas letras tiene el nombre del país.
+
+function counterCountry() {
+    let repeat = prompt("¿cuántas veces quieres que se repita?");
+
+    if (repeat == null || repeat == "" || !Number.isInteger(Number(repeat))) {
+         alert("Ingrese información valida");
+    } else {
+
+         let paises = [];
+         for (let index = 0; index < repeat; index++) {
+              let pais = prompt("Ingrese el nombre del país");
+              if (pais == null || pais == "") {
+                   alert("Ingrese información valida");
+
+              } else {
+                   paises[index] = pais;
+              }
+         }
+
+         for (let index = 0; index < paises.length; index++) {
+
+              alert("La palabra: " + paises[index] + " tiene " + paises[index].length + " letras")
+
+         }
+    }
+
+}
